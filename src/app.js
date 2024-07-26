39dcd4576bdb3e04813342d8ed0dd3e4042093cf
Miguel Navas <143790463+menr-25@users.noreply.github.com>
2024-07-26 20:14:42 +0000

    Proyecto finalizado sin extras

--- src/app.js
+++ src/app.js
@@ -6,26 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let valorCartas = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A"
-  ];
+  let valorCartas = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
   let palos = ["♦", "♥", "♠", "♣"];
   let paloRandom = palos[Math.floor(Math.random() * palos.length)];
 
-  document.querySelector(".carta").innerHTML =
-    valorCartas[Math.floor(Math.random() * valorCartas.length)];
-  document.querySelector(".palo1").innerHTML = paloRandom;
-  document.querySelector(".palo2").innerHTML = paloRandom;
+  document.querySelector(".carta").innerHTML = valorCartas[Math.floor(Math.random() * valorCartas.length)];
+  document.querySelector("#palo1").innerHTML = paloRandom;
+  document.querySelector("#palo2").innerHTML = paloRandom;
+
+  if (paloRandom === "♦" || paloRandom === "♥"){
+    document.querySelector("#palo1").style.color = "red";
+    document.querySelector("#palo2").style.color = "red";
+  };
 };
