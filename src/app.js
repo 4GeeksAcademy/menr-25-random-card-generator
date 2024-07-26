21d9d2e3eb93028cc7070acb4016197689780027
Miguel Navas <143790463+menr-25@users.noreply.github.com>
2024-07-26 16:46:33 +0000

    Primera parte del proyecto HTML y JS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let valorCartas = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let palos = ["♦", "♥", "♠", "♣"];
+  let paloRandom = palos[Math.floor(Math.random() * palos.length)];
+
+  document.querySelector(".carta").innerHTML =
+    valorCartas[Math.floor(Math.random() * valorCartas.length)];
+  document.querySelector(".palo1").innerHTML = paloRandom;
+  document.querySelector(".palo2").innerHTML = paloRandom;
 };
